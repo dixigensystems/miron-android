d108464fba83e5ee9cd77ca1eaee128e4d923e34
DIXIGEN <dixigen.system@gmail.com>
2026-08-18 10:17:30 +0300

    Update MainActivity.java

--- app/src/main/java/ru/dixigen/miron/MainActivity.java
+++ app/src/main/java/ru/dixigen/miron/MainActivity.java
@@ -2,8 +2,8 @@ package ru.dixigen.miron;
 import android.Manifest;import android.app.Activity;import android.content.Intent;import android.content.pm.PackageManager;import android.net.Uri;import android.os.Bundle;import android.speech.tts.TextToSpeech;import android.webkit.*;import java.util.Locale;
 public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
 private WebView web;private TextToSpeech tts;
-protected void onCreate(Bundle b){super.onCreate(b);web=new WebView(this);setContentView(web);tts=new TextToSpeech(this,this);
-WebSettings s=web.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setMediaPlaybackRequiresUserGesture(false);
+protected void onCreate(Bundle b){super.onCreate(b);web=new WebView(this);setContentView(web);web.clearCache(true);CookieManager.getInstance().setAcceptCookie(true);tts=new TextToSpeech(this,this);
+WebSettings s=web.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setMediaPlaybackRequiresUserGesture(false);s.setCacheMode(WebSettings.LOAD_DEFAULT);
 web.addJavascriptInterface(new Bridge(),"Miron");
 web.setWebViewClient(new WebViewClient(){public boolean shouldOverrideUrlLoading(WebView v,String u){if(!u.contains("miron.dixigen.ru")){startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(u)));return true;}return false;}
 public void onPageFinished(WebView v,String u){v.evaluateJavascript("var x=document.createElement('script');x.src='https://miron.dixigen.ru/agent/mshim.js';document.head.appendChild(x);",null);}});
